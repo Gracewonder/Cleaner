29a7e790cdb101088c5b89d23baad0afdeaf9c87
Hvhudu <shtormbreker2@gmail.com>
2021-07-18 19:57:15 +0300

    Внесены изменения в меню

--- Cleaner/Menu.cs
+++ Cleaner/Menu.cs
@@ -14,26 +14,10 @@ namespace Cleaner
         public void EnterChoice()
         {
             Console.ForegroundColor=ConsoleColor.Green;
-
-            int choice;
+            
             Console.WriteLine("1 - Полная очистка кэша");
             Console.WriteLine("2 - Очистка кэша определённого браузера");
             Console.WriteLine("3 - Выход");
-            choice = Convert.ToInt32(Console.ReadLine());
-
-            switch (choice)
-            {
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3 :
-                    break;
-                default:
-                    Console.ForegroundColor=ConsoleColor.Red;
-                    Console.WriteLine("Некорректный ввод! Выберите заново!");
-                    break;
-            }
         }
         public void EnterError()
         {

e6244ed036233d259fd0430d49a8ee10719a16f9
1 <1>
2021-07-18 18:05:26 +0300

    Инициализация проекта

---
+++ Cleaner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
